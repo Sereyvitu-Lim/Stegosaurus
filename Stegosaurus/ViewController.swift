ae063ec03777693253790e4c65f9fce9ec108f01
Sereyvitu Lim <Sereyvitu Lim>
2018-10-15 09:37:07 -0400

    Initial Commit

---
+++ Stegosaurus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stegosaurus
+//
+//  Created by Sereyvitu Lim on 10/15/18.
+//  Copyright © 2018 com.SereyvituLim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
